718841307b6dfe0a95fc738fe7abc38b8e9def6b
Dominik Korittki <23359034+dkorittki@users.noreply.github.com>
2020-12-07 16:03:19 +0100

    Change api group to av.mittwald.de

--- pkg/apis/av/v1beta1/doc.go
+++ pkg/apis/av/v1beta1/doc.go
@@ -1,4 +1,4 @@
 // Package v1beta1 contains API Schema definitions for the av v1beta1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=av.mittwald.systems
+// +groupName=av.mittwald.de
 package v1beta1
